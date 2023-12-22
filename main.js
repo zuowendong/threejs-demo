9efb9a8ee6610cae2f936a632a53e971cedbffef
zwd <17326195691@163.com>
2023-12-22 09:52:28 +0800

    feat: lil-gui

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { GUI } from "three/examples/jsm/libs/lil-gui.module.min.js";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -25,6 +26,7 @@ const cube = new THREE.Mesh(geometry, material);
 const parent_material = new THREE.MeshBasicMaterial({
   color: "#FFE4E1",
 });
+parent_material.wireframe = true;
 const parent_cube = new THREE.Mesh(geometry, parent_material);
 parent_cube.add(cube);
 
@@ -52,3 +54,51 @@ function animate() {
   renderer.render(scene, camera);
 }
 animate();
+
+let eventObj = {
+  Fullscreen: () => {
+    document.body.requestFullscreen();
+  },
+  ExitFullscreen: () => {
+    document.exitFullscreen();
+  },
+};
+
+// 创建gui
+const gui = new GUI();
+// 添加按钮
+gui.add(eventObj, "Fullscreen").name("全屏");
+gui.add(eventObj, "ExitFullscreen").name("退出全屏");
+// 立方体位置
+let folder = gui.addFolder("立方体位置");
+folder
+  .add(cube.position, "x")
+  .min(-10)
+  .max(10)
+  .step(1)
+  .name("立方体x轴位置")
+  .onChange((val) => {
+    console.log(val);
+  });
+folder
+  .add(cube.position, "y")
+  .min(-10)
+  .max(10)
+  .step(1)
+  .name("立方体y轴位置")
+  .onFinishChange((val) => {
+    console.log(val);
+  });
+folder.add(cube.position, "z").min(-10).max(10).step(1).name("立方体z轴位置");
+
+gui.add(parent_material, "wireframe").name("父元素线框模式");
+
+let colorParams = {
+  cubeColor: "#c4b400",
+};
+gui
+  .addColor(colorParams, "cubeColor")
+  .name("立方体颜色")
+  .onChange((val) => {
+    cube.material.color.set(val);
+  });
